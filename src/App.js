702acb3560118e759889e8158c2007616aa70b21
shubham kumar <clboy768@gmail.com>
2023-05-26 12:41:08 +0530

    added notification and handle error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
+import { useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial=true;
 
 function App() {
+  const dispatch=useDispatch()
+   const showCart= useSelector(state=>state.ui.cartIsVisible);
+   const cart=useSelector(state=>state.cart)
+   const notification = useSelector(state=>state.ui.notification)
 
-   const showCart= useSelector(state=>state.ui.cartIsVisible)
+   useEffect(()=>{
+    const sendCartData=async()=>{
+      dispatch(uiActions.showNotification({
+        status:'pending',
+        title:'Sending...',
+        message:' Sending cart data!'
+      }))
+    const response=  await fetch('https://redux-store-882ed-default-rtdb.firebaseio.com/cart.json',{
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      })
+      if(!response.ok){
+        throw new Error ('Sending cart data failed')
+       
+      }
+      // const data=await response.json()
+
+      dispatch(uiActions.showNotification({
+        status:'success',
+        title:'Success!',
+        message:' Sent cart data successfully!'
+      }))
+    }
+    if(isInitial){
+      isInitial=false
+      return;
+    }
+    sendCartData().catch(error=>{
+      dispatch(uiActions.showNotification({
+        status:'error',
+        title:'Error!',
+        message:' Sending cart data failed!'
+      }))
+    })
+   },[cart,dispatch])
   return (
+    <>
     <Layout>
+     {notification && <Notification status={notification.status} title={notification.title} message={notification.message}/> }
     {showCart &&  <Cart />}
       <Products />
     </Layout>
+    </>
   );
 }
 
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,19 +1,46 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
-  import { useDispatch } from 'react-redux';
+  import { useDispatch,useSelector } from 'react-redux';
   import { cartActions } from '../../store/cart-slice';
 
 const ProductItem = (props) => {
+  // const cart=useSelector((state)=>state.cart)
   const dispatch=useDispatch()
   const { title, price, description,id } = props;
   console.log("titile",title)
 
   const addToCartHandler=()=>{
-    dispatch(cartActions.addItemToCart({
-      id: id,
-      title,
-      price
-    }))
+    // const newTotalQuantity= cart.totalQuantity+1;
+    // const updatedItems=cart.items.slice();
+    // const existingItem= updatedItems.find(item=>item.id===id);
+    // if(existingItem) {
+    //   const updatedItem={...existingItem}
+    //   updatedItem.quantity++;
+    //   updatedItem.price=updatedItem.price+price;
+    //   const existingItemIndex=updatedItems.findIndex(item=>item.id===id);
+    //   updatedItems[existingItemIndex] = updatedItem;
+    // }else{
+    //   updatedItems.push({
+    //     id:id,
+    //     price:price,
+    //     quantity:1,
+    //     totalPrice:price,
+    //     name:title
+    //   });
+    //   const newCart={
+    //     totalQuantity:newTotalQuantity,
+    //     items:updatedItems,
+    //   }
+    //   dispatch(cartActions.replaceCart(newCart));
+    // }
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price
+     
+    })
+    )
   }
   return (
     <li className={classes.item}>
